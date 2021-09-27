f9bb92dda549377178de804e93dca6edd23c2fde
HeinHtetAung71201 <htethein609@gmail.com>
2021-09-27 21:31:57 +0630

    sec

--- _project/js/app.js
+++ _project/js/app.js
@@ -1,12 +1,4 @@
-wow = new WOW(
-    {
-    boxClass:     'wow',      // default
-    animateClass: 'animate__animated', // default
-    offset:       0,          // default
-    mobile:       true,       // default
-    live:         true        // default
-    }
-    )
+new WOW().init();
 
 $(document).ready(function(){
    $(window).scroll(function() {
